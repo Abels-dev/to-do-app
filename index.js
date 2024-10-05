558d91f7e574156bc756d9def26439b8f70df7eb
abel <abelgetachew143@gmail.com>
2024-10-05 22:16:14 +0300

    worked on displaying tasks based on their status

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const addedTask = document.getElementById("input-task");
 const taskContainer = document.getElementById("task-container");
-
+const tasksLeft = document.getElementById("item-left");
+const statusButtons = document.querySelectorAll(".statusBtn");
 const taskList = [];
 
-const addTasks = () => {
-   taskContainer.innerHTML += ` <div class="task" id=${taskList.length}>
+const renderTask = (id, text, isCompleted) => {
+   taskContainer.innerHTML += ` <div class="task ${
+      isCompleted ? "checked" : ""
+   }" id=${id}>
                   <label>
                      <input type="checkbox" />
                      <img src="images/icon-check.svg" />
                   </label>
-                  <p>${addedTask.value}</p>
+                  <p>${text}</p>
                   <button class="remove-btn"><img src="images/icon-cross.svg" alt="delete-btn"></button>
                </div>`;
+};
+const countActiveTasks = () => {
+   let count = 0;
+   taskList.forEach((el) => {
+      if (!el.isCompleted) count++;
+   });
+   return count;
+};
+const addTasks = () => {
+   renderTask(taskList.length, addedTask.value, false);
    taskList.push({ task: addedTask.value, isCompleted: false });
+   tasksLeft.innerText = countActiveTasks();
    addedTask.value = "";
 };
-
 addedTask.addEventListener("keydown", (e) => {
    if (e.key === "Enter") addTasks();
 });
@@ -27,11 +40,40 @@ taskContainer.addEventListener("change", (e) => {
       taskEl.classList.add("checked");
       const id = taskEl.id;
       taskList[id].isCompleted = true;
+      tasksLeft.innerText = countActiveTasks();
    } else {
       const label = e.target.parentElement;
       const taskEl = label.parentElement;
       taskEl.classList.remove("checked");
       const id = taskEl.id;
       taskList[id].isCompleted = false;
+      tasksLeft.innerText = countActiveTasks();
    }
+});
+
+const getTasks = (status) => {
+   taskContainer.innerHTML = "";
+   if (status === "All") {
+      taskList.forEach((el, index) => {
+         if (el.isCompleted) renderTask(index, el.task, true);
+         else renderTask(index, el.task, false);
+      });
+   }
+   if (status === "Completed") {
+      taskList.forEach((el, index) => {
+         if (el.isCompleted) renderTask(index, el.task, true);
+      });
+   }
+   if (status === "Active") {
+      taskList.forEach((el, index) => {
+         if (!el.isCompleted) renderTask(index, el.task, false);
+      });
+   }
+};
+statusButtons.forEach((statusBtn) => {
+   statusBtn.addEventListener("click", () => {
+      statusButtons.forEach((btn) => btn.classList.remove("activeBtn"));
+      statusBtn.classList.add("activeBtn");
+      getTasks(statusBtn.textContent);
+   });
 });
